4f40999c3155790fcdcac00654cf22e8c38aaf17
Simon Kreuzpointner <smonman7@gmail.com>
2019-05-23 17:47:44 +0200

    Add project files.

---
+++ Dithering_0/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Dithering_0 {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.pictureBox_display = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.checkBox_grayscale = new System.Windows.Forms.CheckBox();
+			this.checkBox_showEveryStep = new System.Windows.Forms.CheckBox();
+			this.checkBox_dither = new System.Windows.Forms.CheckBox();
+			this.button_saveImage = new System.Windows.Forms.Button();
+			this.button_dither = new System.Windows.Forms.Button();
+			this.button_loadImage = new System.Windows.Forms.Button();
+			this.backgroundWorker_ditherer = new System.ComponentModel.BackgroundWorker();
+			this.toolTip_main = new System.Windows.Forms.ToolTip(this.components);
+			this.numericUpDown_factor = new System.Windows.Forms.NumericUpDown();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.listBox_output = new System.Windows.Forms.ListBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_display)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_factor)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.pictureBox_display, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 151F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 661);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// pictureBox_display
+			// 
+			this.pictureBox_display.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox_display.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox_display.Name = "pictureBox_display";
+			this.pictureBox_display.Size = new System.Drawing.Size(578, 504);
+			this.pictureBox_display.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox_display.TabIndex = 0;
+			this.pictureBox_display.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.groupBox3);
+			this.groupBox1.Controls.Add(this.groupBox2);
+			this.groupBox1.Controls.Add(this.button_saveImage);
+			this.groupBox1.Controls.Add(this.button_dither);
+			this.groupBox1.Controls.Add(this.button_loadImage);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox1.Location = new System.Drawing.Point(3, 513);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(578, 145);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Controls";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.numericUpDown_factor);
+			this.groupBox2.Controls.Add(this.checkBox_grayscale);
+			this.groupBox2.Controls.Add(this.checkBox_showEveryStep);
+			this.groupBox2.Controls.Add(this.checkBox_dither);
+			this.groupBox2.Location = new System.Drawing.Point(9, 48);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(237, 88);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Options";
+			// 
+			// checkBox_grayscale
+			// 
+			this.checkBox_grayscale.AutoSize = true;
+			this.checkBox_grayscale.Location = new System.Drawing.Point(6, 65);
+			this.checkBox_grayscale.Name = "checkBox_grayscale";
+			this.checkBox_grayscale.Size = new System.Drawing.Size(73, 17);
+			this.checkBox_grayscale.TabIndex = 2;
+			this.checkBox_grayscale.Text = "Grayscale";
+			this.checkBox_grayscale.UseVisualStyleBackColor = true;
+			// 
+			// checkBox_showEveryStep
+			// 
+			this.checkBox_showEveryStep.AutoSize = true;
+			this.checkBox_showEveryStep.Location = new System.Drawing.Point(6, 42);
+			this.checkBox_showEveryStep.Name = "checkBox_showEveryStep";
+			this.checkBox_showEveryStep.Size = new System.Drawing.Size(108, 17);
+			this.checkBox_showEveryStep.TabIndex = 1;
+			this.checkBox_showEveryStep.Text = "Show Every Step";
+			this.toolTip_main.SetToolTip(this.checkBox_showEveryStep, "Shows every step in the process of compressing.\r\nThis is very time and RAM consum" +
+        "ing.");
+			this.checkBox_showEveryStep.UseVisualStyleBackColor = true;
+			// 
+			// checkBox_dither
+			// 
+			this.checkBox_dither.AutoSize = true;
+			this.checkBox_dither.Location = new System.Drawing.Point(6, 19);
+			this.checkBox_dither.Name = "checkBox_dither";
+			this.checkBox_dither.Size = new System.Drawing.Size(54, 17);
+			this.checkBox_dither.TabIndex = 0;
+			this.checkBox_dither.Text = "Dither";
+			this.toolTip_main.SetToolTip(this.checkBox_dither, "Also dithers the loaded image.");
+			this.checkBox_dither.UseVisualStyleBackColor = true;
+			// 
+			// button_saveImage
+			// 
+			this.button_saveImage.Location = new System.Drawing.Point(171, 19);
+			this.button_saveImage.Name = "button_saveImage";
+			this.button_saveImage.Size = new System.Drawing.Size(75, 23);
+			this.button_saveImage.TabIndex = 2;
+			this.button_saveImage.Text = "Save Image";
+			this.toolTip_main.SetToolTip(this.button_saveImage, "Saves the result image.");
+			this.button_saveImage.UseVisualStyleBackColor = true;
+			this.button_saveImage.Click += new System.EventHandler(this.button_saveImage_Click);
+			// 
+			// button_dither
+			// 
+			this.button_dither.Location = new System.Drawing.Point(90, 19);
+			this.button_dither.Name = "button_dither";
+			this.button_dither.Size = new System.Drawing.Size(75, 23);
+			this.button_dither.TabIndex = 1;
+			this.button_dither.Text = "Compress";
+			this.toolTip_main.SetToolTip(this.button_dither, "Compresses and if checked dithers the loaded image.");
+			this.button_dither.UseVisualStyleBackColor = true;
+			this.button_dither.Click += new System.EventHandler(this.button_dither_Click);
+			// 
+			// button_loadImage
+			// 
+			this.button_loadImage.Location = new System.Drawing.Point(9, 19);
+			this.button_loadImage.Name = "button_loadImage";
+			this.button_loadImage.Size = new System.Drawing.Size(75, 23);
+			this.button_loadImage.TabIndex = 0;
+			this.button_loadImage.Text = "Load Image";
+			this.toolTip_main.SetToolTip(this.button_loadImage, "Loads an image.");
+			this.button_loadImage.UseVisualStyleBackColor = true;
+			this.button_loadImage.Click += new System.EventHandler(this.button_loadImage_Click);
+			// 
+			// backgroundWorker_ditherer
+			// 
+			this.backgroundWorker_ditherer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_ditherer_DoWork);
+			this.backgroundWorker_ditherer.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_ditherer_RunWorkerCompleted);
+			// 
+			// numericUpDown_factor
+			// 
+			this.numericUpDown_factor.Location = new System.Drawing.Point(114, 16);
+			this.numericUpDown_factor.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			this.numericUpDown_factor.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown_factor.Name = "numericUpDown_factor";
+			this.numericUpDown_factor.Size = new System.Drawing.Size(70, 20);
+			this.numericUpDown_factor.TabIndex = 3;
+			this.toolTip_main.SetToolTip(this.numericUpDown_factor, "The higher the more colors are allowed. That means the\r\nimage file size will also" +
+        " be higher but the image is\r\nmore recognizeable.");
+			this.numericUpDown_factor.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox3.Controls.Add(this.listBox_output);
+			this.groupBox3.Location = new System.Drawing.Point(252, 19);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(320, 117);
+			this.groupBox3.TabIndex = 4;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Infos";
+			// 
+			// listBox_output
+			// 
+			this.listBox_output.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox_output.FormattingEnabled = true;
+			this.listBox_output.Location = new System.Drawing.Point(3, 16);
+			this.listBox_output.Name = "listBox_output";
+			this.listBox_output.SelectionMode = System.Windows.Forms.SelectionMode.None;
+			this.listBox_output.Size = new System.Drawing.Size(314, 98);
+			this.listBox_output.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 661);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.MinimumSize = new System.Drawing.Size(600, 700);
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "Compress Images";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_display)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_factor)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.PictureBox pictureBox_display;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button_saveImage;
+		private System.Windows.Forms.Button button_dither;
+		private System.Windows.Forms.Button button_loadImage;
+		private System.ComponentModel.BackgroundWorker backgroundWorker_ditherer;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.CheckBox checkBox_dither;
+		private System.Windows.Forms.CheckBox checkBox_showEveryStep;
+		private System.Windows.Forms.ToolTip toolTip_main;
+		private System.Windows.Forms.CheckBox checkBox_grayscale;
+		private System.Windows.Forms.NumericUpDown numericUpDown_factor;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.ListBox listBox_output;
+	}
+}
+
---
+++ Dithering_0/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Dithering_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        Bitmap workingImage;
+        Bitmap outputImage;
+        int factor = 1;
+        bool showEveryStep;
+        bool dither;
+        bool grayscale;
+        long oldFileSize;
+        long newFileSize;
+        FileInfo info;
+        string oldFileName;
+        string newFileName;
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_loadImage.Enabled = true;
+            button_dither.Enabled = false;
+            button_saveImage.Enabled = false;
+            checkBox_dither.Enabled = false;
+            checkBox_showEveryStep.Enabled = false;
+            checkBox_grayscale.Enabled = false;
+            numericUpDown_factor.Enabled = false;
+            Clear();
+        }
+
+        private void button_loadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Image";
+            ofd.Filter = "Image Files | *.png; *.jpeg; *.jpg; *.bmp; *.tiff";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Clear();
+                info = new FileInfo(ofd.FileName);
+                oldFileSize = info.Length;
+                oldFileName = Path.GetFileNameWithoutExtension(ofd.FileName);
+                pictureBox_display.Image = new Bitmap(ofd.FileName);
+                button_loadImage.Enabled = true;
+                button_dither.Enabled = true;
+                button_saveImage.Enabled = true;
+                checkBox_dither.Enabled = true;
+                checkBox_showEveryStep.Enabled = true;
+                checkBox_grayscale.Enabled = true;
+                numericUpDown_factor.Enabled = true;
+                StatusUpdate("Image loaded successfully.");
+                StatusUpdate("Old file size: " + oldFileSize + " bytes.");
+            }
+        }
+
+        private void button_dither_Click(object sender, EventArgs e)
+        {
+            workingImage = new Bitmap(pictureBox_display.Image);
+            outputImage = new Bitmap(workingImage.Width, workingImage.Height, PixelFormat.Format16bppArgb1555);
+            dither = checkBox_dither.Checked;
+            showEveryStep = checkBox_showEveryStep.Checked;
+            grayscale = checkBox_grayscale.Checked;
+            factor = (int)numericUpDown_factor.Value;
+
+            backgroundWorker_ditherer.RunWorkerAsync();
+
+            button_loadImage.Enabled = false;
+            button_dither.Enabled = false;
+            button_saveImage.Enabled = false;
+            checkBox_dither.Enabled = false;
+            checkBox_showEveryStep.Enabled = false;
+            checkBox_grayscale.Enabled = false;
+            numericUpDown_factor.Enabled = false;
+        }
+
+        private void button_saveImage_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save Image";
+            sfd.Filter = "png(*.png) | *.png | jpeg(*.jpeg) | *.jpeg | jpg(*.jpg) | *.jpg | bmp(*.bmp) | *.bmp | tiff(*.tiff) | *.tiff";
+            ImageFormat imgFormat;
+            newFileName = oldFileName + "_compressed";
+            if (dither)
+            {
+                newFileName += "_dithered_factor_" + factor;
+            }
+            if (grayscale)
+            {
+                newFileName += "_grayscale";
+            }
+            sfd.FileName = newFileName;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string extension = Path.GetExtension(sfd.FileName);
+                switch (extension)
+                {
+                    case ".png":
+                        imgFormat = ImageFormat.Png;
+                        break;
+                    case ".jpeg":
+                        imgFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        imgFormat = ImageFormat.Bmp;
+                        break;
+                    case ".tiff":
+                        imgFormat = ImageFormat.Tiff;
+                        break;
+                    default:
+                        imgFormat = ImageFormat.Png;
+                        break;
+                }
+                pictureBox_display.Image.Save(sfd.FileName, imgFormat);
+                info = new FileInfo(sfd.FileName);
+                newFileSize = info.Length;
+                StatusUpdate("Image saved successfully.");
+                StatusUpdate("New file size: " + oldFileSize + " bytes.");
+            }
+        }
+
+        private void backgroundWorker_ditherer_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            int newR;
+            int newG;
+            int newB;
+            int errorR;
+            int errorG;
+            int errorB;
+            int r;
+            int g;
+            int b;
+
+            Color c;
+            Color tempColor;
+
+            int gray;
+
+            int[][] indexes;
+            int[] factors = new int[] { 7, 3, 5, 1 };
+
+            StatusUpdate("Started compressing.");
+
+            for (int y = 0; y < workingImage.Height - 1; y++)
+            {
+                for (int x = 1; x < workingImage.Width - 1; x++)
+                {
+
+                    if (grayscale)
+                    {
+                        c = workingImage.GetPixel(x, y);
+                        gray = (int)((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11)); //for the numbers check: https://en.wikipedia.org/wiki/Grayscale and https://stackoverflow.com/questions/2265910/convert-an-image-to-grayscale
+                        workingImage.SetPixel(x, y, Color.FromArgb(c.A, gray, gray, gray));
+                        outputImage.SetPixel(x, y, Color.FromArgb(c.A, gray, gray, gray));
+                    }
+
+                    c = workingImage.GetPixel(x, y);
+                    newR = Convert.ToInt32(factor * (float)c.R / 255) * (255 / factor);
+                    newG = Convert.ToInt32(factor * (float)c.G / 255) * (255 / factor);
+                    newB = Convert.ToInt32(factor * (float)c.B / 255) * (255 / factor);
+                    outputImage.SetPixel(x, y, Color.FromArgb(c.A, newR, newG, newB));
+                    if (dither)
+                    {
+                        errorR = c.R - newR;
+                        errorG = c.G - newG;
+                        errorB = c.B - newB;
+                        indexes = new int[][]{
+                            new int[]{ x + 1, y    },
+                            new int[]{ x - 1, y + 1},
+                            new int[]{ x    , y + 1},
+                            new int[]{ x + 1, y + 1},
+                        };
+                        for (int i = 0; i < 4; i++)
+                        {
+                            tempColor = workingImage.GetPixel(indexes[i][0], indexes[i][1]);
+                            r = tempColor.R;
+                            g = tempColor.G;
+                            b = tempColor.B;
+                            r += Convert.ToInt32((float)errorR * factors[i] / 16);
+                            g += Convert.ToInt32((float)errorG * factors[i] / 16);
+                            b += Convert.ToInt32((float)errorB * factors[i] / 16);
+
+                            r = Clamp(r);
+                            g = Clamp(g);
+                            b = Clamp(b);
+
+                            workingImage.SetPixel(indexes[i][0], indexes[i][1], Color.FromArgb(tempColor.A, r, g, b));
+                            outputImage.SetPixel(indexes[i][0], indexes[i][1], Color.FromArgb(tempColor.A, r, g, b));
+                        }
+                    }
+                }
+                if (showEveryStep)
+                {
+                    pictureBox_display.Invoke((MethodInvoker)(() => pictureBox_display.Image.Dispose()));
+                    pictureBox_display.Invoke((MethodInvoker)(() => pictureBox_display.Image = new Bitmap(outputImage)));
+                }
+            }
+
+            StatusUpdate("Finished compressing.");
+        }
+
+        private void backgroundWorker_ditherer_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            pictureBox_display.Image.Dispose();
+            pictureBox_display.Image = new Bitmap(outputImage);
+
+            button_loadImage.Enabled = true;
+            button_dither.Enabled = true;
+            button_saveImage.Enabled = true;
+            checkBox_dither.Enabled = true;
+            checkBox_showEveryStep.Enabled = true;
+            checkBox_grayscale.Enabled = true;
+            numericUpDown_factor.Enabled = true;
+        }
+
+        void Clear()
+        {
+            pictureBox_display.Image = null;
+            listBox_output.Items.Clear();
+            oldFileName = "";
+            newFileName = "";
+        }
+
+        int Clamp(int input)
+        {
+            if (input < 0)
+            {
+                return 0;
+            }
+            else if (input > 255)
+            {
+                return 255;
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        void StatusUpdate(string message)
+        {
+            listBox_output.Invoke((MethodInvoker)(() => listBox_output.Items.Add(message)));
+        }
+    }
+}
